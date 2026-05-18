59e20e3f31bb8b72774829e13b05694cd4f865e1
thiago-p23 <thi23112005ago@gmail.com>
2026-05-18 16:23:09 -0300

    Update Lista_de_convidados.java

--- src/Lista_de_convidados.java
+++ src/Lista_de_convidados.java
@@ -5,13 +5,11 @@ void main() {
     int quantidade = Integer.parseInt(readln("Digite a quantidade de convidados desejados: "));
     String[] convidados = new String[quantidade];
 
-    // Cadastro
     for (int i = 0; i < convidados.length; i++) {
         convidados[i] = readln("Digite o nome do " + (i + 1) + "° convidado: ");
     }
     println("\n===== LISTA DE CONVIDADOS =====");
-
-    // Exibição
+    
     for (int i = 0; i < convidados.length; i++) {
         println((i + 1) + " - " + convidados[i]);
     }
